1ed2e11b0d7bc49422002b707c02f215bf7b797b
ibrahima.diallo1@yahoo.com <ibrahima.diallo1@yahoo.com>
2025-04-26 19:38:50 +0200

    feat: ajout du fichier employes_rh.csv et analyse exploratoire

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
 #import de pandas pour la manipulation de donnèes
 import pandas as pd 
 import streamlit as st
+#lecture du fichier
+df = pd.read_csv("data/employes_rh.csv",index_col=None)
+#affichage des premieres lignes
+print(df.head())
 
-#Titre de l'application
-st.title('Nettoyage de donnèes RH')
+#stat
 
-#upload du fichier
-upload_file = st.file_uploader("Choisissez un fichier csv",type="csv")
-if upload_file is not None:
-
- 
-    #lecture du fichier csv
-    df = pd.read_csv(upload_file,index_col=None)
-    #apperçu des 5 premiers lignes
-    st.subheader('apperçu des donnèes')
-    st.dataframe(df.head())
-    #statistiques simples
-    st.subheader('statistiques')
-    st.write("Age moyen : ",df.age.mean())
-    st.write("Postes les plus fréquents : ",df.poste.value_counts())
-    #suppression des doublons
-    df_cleaned = df.drop_duplicates()
-
-    st.subheader('Données nettoyées')
-    st.dataframe(df_cleaned.head())
-
-    #télécharger le fichier néttoyé
-    st.download_button(
-        label="Télécharger le fichier nettoyé",
-            data = df_cleaned.to_csv(index=False).encode('utf-8'),
-        file_name= "employes_nettoyes.csv",
-           mime='text/csv'   )   
+print("age moyen :",round(df.age.mean()),"ans")
+print("Taux de départ : ",round(df.depart.mean()*100),"%")
+print("salaire moyen : ",round(df.salaire.mean()),"€")
