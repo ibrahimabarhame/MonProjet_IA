ba95ebef3e445f1c59b55452a8e3475c99b9f57c
ibrahima.diallo1@yahoo.com <ibrahima.diallo1@yahoo.com>
2025-05-02 00:16:58 +0200

    feat: intégration du modèle IA dans l'app Streamlit

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 #import de pandas pour la manipulation de donnèes
 import pandas as pd 
 import streamlit as st
-#lecture du fichier
-df = pd.read_csv("data/employes_rh.csv",index_col=None)
+import joblib
+
 #affichage des premieres lignes
-print(df.head())
+model = joblib.load('model/model_turn_over.pkl')
 
-#stat
+st.title("Prédiction du risque de départ des employés")
 
-print("age moyen :",round(df.age.mean()),"ans")
-print("Taux de départ : ",round(df.depart.mean()*100),"%")
-print("salaire moyen : ",round(df.salaire.mean()),"€")
+upload_file = st.file_uploader("choisissez vottre fichier",type="csv")
+
+if upload_file is not None :
+
+    df = pd.read_csv(upload_file,index_col=None)
+    st.subheader("Apperçu des donnèes")
+    st.dataframe(df.head())
+
+    #preparation des donnèes pour prediction
+
+    colonnes_a_garder = [col for col in df.columns if col != 'depart']
+
+    X = df[colonnes_a_garder]
+
+    predictions = model.predict(X)
+
+    df["Risque_depart"] = predictions
+
+    st.subheader("Prediction")
+    st.dataframe(df)
+
+    st.download_button(
+        label="Télécharger le fichier avec prédictions",
+        data=df.to_csv(index=False).encode('utf-8'),
+        file_name='employes_avec_risque.csv',
+        mime='text/csv'
+    )
